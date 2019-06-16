bdd3f3f9b91ab1311464066e8845a88b76bb2419
Vale <valeria_luce@hotmail.com>
2019-06-16 14:43:11 +0200

    set up of ViewDidLoad in ReflectionVC

--- Onigiri/ReflectionDetailViewController.swift
+++ Onigiri/ReflectionDetailViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreData
 
-class ReflectionDetailViewController: UIViewController {
+class ReflectionDetailViewController: UIViewController, UITextViewDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var part1View: UIView!
     @IBOutlet weak var part2View: UIView!
@@ -25,14 +26,73 @@ class ReflectionDetailViewController: UIViewController {
     @IBOutlet weak var answer3TextView: UITextView!
     @IBOutlet weak var answer4TextView: UITextView!
     
+    let question1 = "erstens"
+    let question2 = "zweitens"
+    let question3 = "drittens"
+    let question4 = "viertens"
     
     
+    var managedObjectContext: NSManagedObjectContext? {
+        return (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    }
+    
+    var reflectionsFetchedResultsController: NSFetchedResultsController<Reflection>!
+    var reflections = [Reflection]()
+    var reflection: Reflection?
+    var isExsisting = false
+    var indexPath: Int?
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // set questions
+        question1Label.text = question1
+        question2Label.text = question2
+        question3Label.text = question3
+        question4Label.text = question4
+        
+        // Load data
+        if let reflection = reflection {
+            answer1TextView.text = reflection.answer1
+            answer2TextView.text = reflection.answer2
+            answer3TextView.text = reflection.answer3
+            answer4TextView.text = reflection.answer4
+        }
+        
+        if question1Label.text != "" {
+            isExsisting = true
+        }
+        
+        
+        // Delegates
+        answer1TextView.delegate = self
+        
+        
+        // Styles
+        part1View.layer.shadowColor =  UIColor(red:0/255.0, green:0/255.0, blue:0/255.0, alpha: 1.0).cgColor
+        part1View.layer.shadowOffset = CGSize(width: 0.75, height: 0.75)
+        part1View.layer.shadowRadius = 1.5
+        part1View.layer.shadowOpacity = 0.2
+        part1View.layer.cornerRadius = 2
+        
+        part2View.layer.shadowColor =  UIColor(red:0/255.0, green:0/255.0, blue:0/255.0, alpha: 1.0).cgColor
+        part2View.layer.shadowOffset = CGSize(width: 0.75, height: 0.75)
+        part2View.layer.shadowRadius = 1.5
+        part2View.layer.shadowOpacity = 0.2
+        part2View.layer.cornerRadius = 2
+        
+        part3View.layer.shadowColor =  UIColor(red:0/255.0, green:0/255.0, blue:0/255.0, alpha: 1.0).cgColor
+        part3View.layer.shadowOffset = CGSize(width: 0.75, height: 0.75)
+        part3View.layer.shadowRadius = 1.5
+        part3View.layer.shadowOpacity = 0.2
+        part3View.layer.cornerRadius = 2
+        
+        part4View.layer.shadowColor =  UIColor(red:0/255.0, green:0/255.0, blue:0/255.0, alpha: 1.0).cgColor
+        part4View.layer.shadowOffset = CGSize(width: 0.75, height: 0.75)
+        part4View.layer.shadowRadius = 1.5
+        part4View.layer.shadowOpacity = 0.2
+        part4View.layer.cornerRadius = 2
     }
     
 
