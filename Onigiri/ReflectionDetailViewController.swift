f6d574ba694bbcf4a6159952b713bb46d875e7a3
Vale <valeria_luce@hotmail.com>
2019-06-16 15:07:01 +0200

    added saves and cancel function

--- Onigiri/ReflectionDetailViewController.swift
+++ Onigiri/ReflectionDetailViewController.swift
@@ -95,8 +95,93 @@ class ReflectionDetailViewController: UIViewController, UITextViewDelegate, UINa
         part4View.layer.cornerRadius = 2
     }
     
+    
+    // Core data
+    func saveToCoreData(completion: @escaping ()->Void){
+        managedObjectContext!.perform {
+            do {
+                try self.managedObjectContext?.save()
+                completion()
+                print("Note saved to CoreData.")
+                
+            } catch let error {
+                print("Could not save note to CoreData: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // Saving Data
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        if answer1TextView.text == "" {
+            let alertController = UIAlertController(title: "Fehlende Information", message: "Bitte beantworte die erste Reflektionsfrage. Die restlichen Reflektionsfragen müssen nicht ausgefüllt werden.", preferredStyle: UIAlertController.Style.alert)
+            let OKAction = UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: nil)
+            
+            alertController.addAction(OKAction)
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            if (isExsisting == false) {
+                let answer1 = answer1TextView.text
+                let answer2 = answer2TextView.text
+                let answer3 = answer3TextView.text
+                let answer4 = answer4TextView.text
+                
+                if let moc = managedObjectContext {
+                    let reflection = Reflection(context: moc)
+                }
+                
+                reflection?.answer1 = answer1
+                reflection?.answer2 = answer2
+                reflection?.answer3 = answer3
+                reflection?.answer4 = answer4
+                
+                saveToCoreData() {
+                    
+                    let isPresentingInAddFluidPatientMode = self.presentingViewController is UINavigationController
+                        if isPresentingInAddFluidPatientMode {
+                            self.dismiss(animated: true, completion: nil)
+                        } else {
+                            self.navigationController!.popViewController(animated: true)
+                        }
+                    }
+                } else if (isExsisting == true) {
+                    let reflection = self.reflection
+                    
+                    let managedObject = reflection
+                    managedObject!.setValue(answer1TextView.text, forKey: "answer1")
+                    managedObject!.setValue(answer2TextView.text, forKey: "answer2")
+                    managedObject!.setValue(answer3TextView.text, forKey: "answer3")
+                    managedObject!.setValue(answer4TextView.text, forKey: "answer4")
+                
+                do {
+                    try context.save()
+                    let isPresentingInAddFluidPatientMode = self.presentingViewController is UINavigationController
+                    if isPresentingInAddFluidPatientMode {
+                        self.dismiss(animated: true, completion: nil)
+                    } else {
+                        self.navigationController!.popViewController(animated: true)
+                    }
+                } catch {
+                    print("Failed to update existing note.")
+                }
+            }
+        }
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddFluidPatientMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddFluidPatientMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController!.popViewController(animated: true)
+        }
+    }
 
-    /*
+    
+    
+    
+
+/*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
