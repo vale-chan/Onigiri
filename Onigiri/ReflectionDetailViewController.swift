aeffb362e4c7cc7ab2ca0ef92b2f8e17abec226e
Vale <valeria_luce@hotmail.com>
2019-06-16 14:30:08 +0200

    created outlets for all views and labels and textViews in VC

---
+++ Onigiri/ReflectionDetailViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ReflectionDetailViewController.swift
+//  Onigiri
+//
+//  Created by Vale-chan on 16.06.19.
+//  Copyright © 2019 Vale-chan. All rights reserved.
+//
+
+import UIKit
+
+class ReflectionDetailViewController: UIViewController {
+    
+    @IBOutlet weak var part1View: UIView!
+    @IBOutlet weak var part2View: UIView!
+    @IBOutlet weak var part3View: UIView!
+    @IBOutlet weak var part4View: UIView!
+    
+    @IBOutlet weak var question1Label: UILabel!
+    @IBOutlet weak var question2Label: UILabel!
+    @IBOutlet weak var question3Label: UILabel!
+    @IBOutlet weak var question4Label: UILabel!
+    
+    @IBOutlet weak var answer1TextView: UITextView!
+    @IBOutlet weak var answer2TextView: UITextView!
+    @IBOutlet weak var answer3TextView: UITextView!
+    @IBOutlet weak var answer4TextView: UITextView!
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
